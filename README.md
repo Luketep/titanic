ddf78d49b4eaad1b766ad9b1d66b76b6fa96fb57
Philip Langbehn <philip.langbehn@daimler.com>
2017-12-08 11:21:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# TITANIC
+
+Do you have problems with an
+
+Aweful
+Idiotic
+Dumb
+Application
+
+? 
+
+Titanic is here to help. Simply add the following code as a bookmark to
+your browser and enjoy!
+
+`javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://raw.github.com/luketep/titanic/master/index.js';document.body.appendChild(s);})();`
+
+### How to add a bookmark
+![alt text](https://raw.github.com/luketep/titanic/master/bookmark.gif "Createing a bookmark in Google Chrome")
---
+++ index.js
@@ -0,0 +1,6 @@
+var container = document.querySelector('.box_freigabe_content');
+container.children[0].children[1].children[0].children[0].children[1].children[0].children[0].children[1].children[0].children[0].value = 'Ko';
+container.children[0].children[1].children[0].children[0].children[1].children[0].children[0].children[1].children[1].children[0].value = '9';
+container.children[0].children[1].children[0].children[0].children[1].children[0].children[0].children[2].children[0].children[0].value = 'Ge';
+container.children[0].children[1].children[0].children[0].children[1].children[0].children[0].children[2].children[1].children[0].value = '17:30';
+container.children[0].children[1].children[0].children[0].children[1].children[0].children[0].children[8].children[0].children[0].value = ' ';
